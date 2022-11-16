36a51802dd69fe7e04a09ca99fe8bbfeb4c8a982
SeokJu Kim <90549862+SoJuSo@users.noreply.github.com>
2022-11-16 14:08:36 +0900

    [Bronze V] Title: 개수 세기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int size = 0;
+    int num = 0;
+    int check_num = 0;
+    int cnt = 0;
+
+    scanf("%d", &size);
+
+    int *arr = (int *)malloc(sizeof(int) * size);
+
+    for (int i = 0; i < size; i++)
+    {
+        scanf("%d", &num);
+        arr[i] = num;
+    }
+
+    scanf("%d", &check_num);
+
+    for (int i = 0; i < size; i++)
+    {
+        if (check_num == arr[i])
+        {
+            cnt++;
+        }
+    }
+
+    printf("%d", cnt);
+
+    return 0;
+}
