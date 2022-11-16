5b2be7cdce95de35bf48ee853b88b728985b7925
SeokJu Kim <90549862+SoJuSo@users.noreply.github.com>
2022-11-16 14:32:11 +0900

    [level 0] Title: 짝수의 합, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n) {
+    int answer = 0;
+    for(int i = 0; i<=n; i=i+2){
+        answer += i;
+    }
+    
+    return answer;
+}
