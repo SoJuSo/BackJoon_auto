aabe71c78bb86a7d69ad9dcf033ae133a7c0602c
SeokJu Kim <90549862+SoJuSo@users.noreply.github.com>
2023-01-17 13:31:13 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -0,0 +1,20 @@
+function solution(dot) {
+    var answer = 0;
+    if(dot[0] > 0 && dot[1] > 0){
+        answer = 1;
+    }
+    
+    if(dot[0] < 0 && dot[1] > 0){
+        answer = 2;
+    }
+    
+    if(dot[0] < 0 && dot[1] < 0){
+        answer = 3;
+    }
+    
+    if(dot[0] > 0 && dot[1] < 0){
+        answer = 4;
+    }
+    
+    return answer;
+}
