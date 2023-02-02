9693953404b664c97458bc5c8c718387b3442c52
SeokJu Kim <90549862+SoJuSo@users.noreply.github.com>
2023-02-02 16:42:23 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -0,0 +1,17 @@
+function solution(age) {
+    var answer = '';
+    let arr = {
+        0 : "a",
+        1 : "b",
+        2 : "c",
+        3 : "d",
+        4 : "e",
+        5 : "f",
+        6 : "g",
+        7 : "h",
+        8 : "i",
+        9 : "j"
+    }
+    answer = String(age).split('').map((i) => arr[i]).join('')
+    return answer;
+}
