6031591c050dcb27739bcb06cd53d75e0e074fd2
SeokJu Kim <90549862+SoJuSo@users.noreply.github.com>
2023-08-11 14:37:43 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    var answer = '';
+    for(let i = 1; i <= n; i++){
+        if(i % 2 === 1){
+            answer += '수'
+        }else{
+            answer += '박'
+        }
+    }
+    return answer;
+}
