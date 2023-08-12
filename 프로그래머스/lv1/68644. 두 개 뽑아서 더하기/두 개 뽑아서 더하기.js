e972e845ead8c0e23aaf828905d91eaf9025ef0b
SeokJu Kim <90549862+SoJuSo@users.noreply.github.com>
2023-08-12 12:33:54 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.39 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -0,0 +1,11 @@
+function solution(numbers) {
+    var answer = [];
+    for(let i = 0; i < numbers.length; i++){
+        for(let j = i + 1; j < numbers.length; j++){
+            if(!(answer.includes(numbers[i] + numbers[j]))){
+                answer.push(numbers[i] + numbers[j])
+            }
+        }
+    }
+    return answer.sort((a,b) => a - b);
+}
