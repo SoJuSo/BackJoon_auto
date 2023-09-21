bedc2d888050c11ed6526cdfbfd93fba48766d87
SeokJu Kim <90549862+SoJuSo@users.noreply.github.com>
2023-09-21 18:20:24 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.04 ms, Memory: 33 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -0,0 +1,13 @@
+function solution(s) {
+    s = s.replaceAll("zero","0")
+    s = s.replaceAll("one","1")
+    s = s.replaceAll("two","2")
+    s = s.replaceAll("three","3")
+    s = s.replaceAll("four","4")
+    s = s.replaceAll("five","5")
+    s = s.replaceAll("six","6")
+    s = s.replaceAll("seven","7")
+    s = s.replaceAll("eight","8")
+    s = s.replaceAll("nine","9")
+    return Number(s);
+}
