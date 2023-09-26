f8cde9cd957083e9036c7943acf4eddbc3a60860
SeokJu Kim <90549862+SoJuSo@users.noreply.github.com>
2023-09-26 11:42:52 +0900

    [level 2] Title: 가장 큰 수, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.js
@@ -2,6 +2,9 @@ function solution(numbers) {
     //아이디어
     //정렬 안에 함수를 잘 쓰자
     let answer = '';
-    answer = numbers.map((val) => String(val)).sort((a,b) => (b+a) - (a+b)).join("")
+    answer = numbers.map((val) => String(val)).sort((a,b) => {
+        // console.log(a+b, ' ' , b+a)
+        return (b + a) - (a + b)
+    }).join("")
     return answer[0] == '0' ? '0' : answer;//0000 같은 경우 예외처리
 }
