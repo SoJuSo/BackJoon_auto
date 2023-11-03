73552d098245768f56cc682c6e4c4c075af7af11
SeokJu Kim <90549862+SoJuSo@users.noreply.github.com>
2023-11-03 17:06:23 +0900

    검색 후 코드 수정

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.js
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.js
@@ -1,16 +1,26 @@
-const input = require('fs').readFileSync('/dev/stdin').toString().trim().split("\n");
+// https://www.acmicpc.net/problem/1003
 
-const len = input.shift();
+let fs = require("fs");
+// let input = fs.readFileSync("0_example.txt").toString().split("\n"); //vscode
+let input = fs.readFileSync("/dev/stdin").toString().split("\n"); //백준
 
-for (let i = 0; i < len; i++) {
-    let oneCount = 0, zeroCount = 1;
-    
-    for (let j = 1; j <= input[i]; j++) {
-        const tmpCount = zeroCount;
-       
-        zeroCount = oneCount;
-        oneCount = tmpCount + oneCount;
-    }
+// console.log(input);
+const t = Number(input.shift());
+const N = input.map(Number);
+// console.log(N);
 
-    console.log(zeroCount + " " + oneCount);   
+for (let i = 0; i < t; i++) {
+  let oneCount = 0,
+    zeroCount = 1;
+
+  for (let j = 1; j <= N[i]; j++) {
+    const tmpCount = zeroCount;
+
+    zeroCount = oneCount;
+    oneCount = tmpCount + oneCount;
+  }
+
+  console.log(zeroCount + " " + oneCount);
 }
+
+// console.log(dp);
