75565786596a1ca750b758c0cfe92f795ea7e1c4
SeokJu Kim <90549862+SoJuSo@users.noreply.github.com>
2023-12-09 23:47:01 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 152 ms, Memory: 9484 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.js
@@ -0,0 +1,15 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+  input.push(line);
+}).on("close", function () {
+  // 여기에 풀이 작성
+  console.log(input[0].length)
+});
