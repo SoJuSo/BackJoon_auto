184bf4d2becca986425a9baf81df913bccecf49c
SeokJu Kim <asdfg7123@gmail.com>
2024-03-16 23:16:57 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 1916 ms, Memory: 10320 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.js
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.js
@@ -7,27 +7,23 @@ const input = fs.readFileSync("/dev/stdin").toString().split("\n"); // 백준
 // console.log(input);
 const [N, K] = [Number(input[0].split(" ")[0]), Number(input[0].split(" ")[1])];
 
-// console.log(N, K);
+const queueArray = new Array(N).fill().map((_, index) => index + 1);
+const answerArray = new Array();
 
-let queueArray = new Array(N).fill().map((_, index) => index + 1);
-let answerArray = new Array();
-
-// console.log(queueArray);
 let answerCounter = 0;
+let currentIndex = 0;
 
-while (1) {
-  queueArray.map((val, index) => {
-    if (val !== 0) answerCounter++;
+while (answerArray.length !== N) {
+  if (queueArray[currentIndex] !== 0) answerCounter++;
+  if (answerCounter % K === 0 && queueArray[currentIndex] !== 0) {
+    answerArray.push(queueArray[currentIndex]);
+    queueArray[currentIndex] = 0;
+    answerCounter = 0;
+  }
 
-    if (answerCounter % K === 0) {
-      if (val !== 0) answerArray.push(queueArray[index]);
-      queueArray[index] = 0;
-      answerCounter = 0;
-    }
-  });
+  currentIndex++;
 
-  // 종료 조건
-  if (answerArray.length === N) break;
+  if (currentIndex === N) currentIndex = 0;
 }
 
 console.log("<" + answerArray.join(", ") + ">");
