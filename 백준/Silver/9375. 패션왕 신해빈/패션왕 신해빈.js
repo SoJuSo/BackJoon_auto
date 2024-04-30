8a5e76d3cb4b91cafdcf20e888acf1c8048c1edb
SeokJu Kim <asdfg7123@gmail.com>
2024-04-30 14:31:46 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 128 ms, Memory: 9912 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.js
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.js
@@ -5,7 +5,6 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "0_example.txt";
 const splitPath = process.platform === "linux" ? "\n" : "\r\n";
 let input = fs.readFileSync(filePath).toString().split(splitPath);
 
-// console.log(input);
 const TestCase = Number(input.shift());
 
 for (let i = 0; i < TestCase; i++) {
@@ -26,36 +25,11 @@ for (let i = 0; i < TestCase; i++) {
     val.push(null);
   });
 
-  //   console.log(result);
-
-  //   let count = 0;
-  //   let combine = result.length > 1 ? 1 : 0;
-
-  // 조합? 순열? 이건 조합문제이다.
-  // 중복없는 점화식 필요?
-  // 경우의 수 문제
-  // 뭘 놓치고 있을까.,.,.,.
-
-  // 핵심 >> 아무것도 입지 않을 수 있는 경우가 추가됌
-  // 따라서 아무것도 입지 않는 경우를 오히려 추가해야함
-  // 근데 모두 null일경우는 빼줘야하니 -1
-
   let answer = 1;
-
-  //   for (const val of Object.values(result)) {
-  //     answer *= val.length;
-  //   }
 
   result.forEach((val) => {
     answer *= val.length;
-    // count += val.length;
-    // combine *= val.length;
-    // console.log(count, combine);
   });
 
   console.log(answer - 1);
-  //   console.log(answer - 1);
-  //   console.log(count + (count * (count - 1)) / 2);
-  //   console.log(clothObject);
-  //   console.log(Object.values(clothObject));
 }
