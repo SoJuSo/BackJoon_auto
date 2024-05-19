fa48ebbc6f08b47bbf9b9f27a7542ce5e4bc7c59
SeokJu Kim <asdfg7123@gmail.com>
2024-05-19 15:41:37 +0900

    [Silver I] Title: IOIOI, Time: 196 ms, Memory: 12160 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.js
+++ 백준/Silver/5525. IOIOI/IOIOI.js
@@ -1,23 +1,51 @@
-//https://www.acmicpc.net/problem/5525
-
 const fs = require("fs");
+// 플랫폼에 따라 입력 파일 경로를 설정
 const filePath = process.platform === "linux" ? "/dev/stdin" : "0_example.txt";
+// 플랫폼에 따라 줄바꿈 문자 설정
 const splitPath = process.platform === "linux" ? "\n" : "\r\n";
+
+// 입력 파일을 읽고, 줄바꿈 문자로 나누어 배열로 저장
 let input = fs.readFileSync(filePath).toString().trim().split(splitPath);
 
-const N = Number(input[0]);
-const M = Number(input[1]);
-const S = input[2];
+const N = Number(input[0]); // 첫 번째 입력: N 값
+const M = Number(input[1]); // 두 번째 입력: M 값
+const S = input[2]; // 세 번째 입력: 문자열 S
 
+// 해결 함수 정의
 const sol = (num, len, str) => {
-  const PN = "IOI" + "OI".repeat(num - 1);
-  let cnt = 0;
-  for (let i = 0; i < len - PN.length + 1; i++) {
-    if (str.slice(i, i + PN.length) === PN) {
-      cnt++;
+  let switchFlag = false; // IO 패턴이 시작됐는지 여부를 저장하는 플래그
+  let count = 1; // 현재 연속된 문자 수를 저장
+  let answer = 0; // 결과값을 저장
+  // N에 따라 IOI 패턴 생성
+  let P = "IO".repeat(num) + "I";
+
+  // 문자열 S를 순회
+  for (let i = 0; i < len; i++) {
+    // switchFlag가 꺼져있고 현재 문자가 'I'인 경우
+    if (!switchFlag && str[i] === "I") {
+      switchFlag = true; // 플래그를 켠다
+      // switchFlag가 켜져있고 현재 문자가 이전 문자와 다른 경우
+    } else if (switchFlag && str[i] !== str[i - 1]) {
+      count += 1; // count를 증가시킨다
+    } else {
+      // 현재 count가 P의 길이 이상인 경우
+      if (count >= P.length) {
+        // 가능한 패턴의 수를 answer에 더한다
+        answer += Math.floor((count - P.length) / 2) + 1;
+      }
+      count = 1; // count를 초기화
+      // 현재 문자가 'O'인 경우 switchFlag를 끈다
+      if (str[i] === "O") switchFlag = false;
     }
   }
-  return cnt;
+
+  // 마지막 부분에서 count가 P의 길이 이상인 경우
+  if (count >= P.length) {
+    // 가능한 패턴의 수를 answer에 더한다
+    answer += Math.floor((count - P.length) / 2) + 1;
+  }
+
+  return answer; // 결과값 반환
 };
 
-console.log(sol(N, M, S));
+console.log(sol(N, M, S)); // 결과 출력
