81d11e5cedd0092d7baf8936984196af42ee2b4f
SeokJu Kim <asdfg7123@gmail.com>
2024-05-30 23:55:11 +0900

    [Bronze V] Title: 큰 수 (BIG), Time: 3384 ms, Memory: 12324 KB -BaekjoonHub

---
+++ 백준/Bronze/14928. 큰 수 （BIG）/큰 수 （BIG）.js
@@ -0,0 +1,8 @@
+//https://www.acmicpc.net/problem/14928
+
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "0_example.txt";
+const splitPath = process.platform === "linux" ? "\n" : "\r\n";
+let input = fs.readFileSync(filePath).toString().trim().split(splitPath);
+
+console.log((BigInt(input[0]) % 20000303n).toString());
