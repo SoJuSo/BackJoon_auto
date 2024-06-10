ca138280fa5e7070309b4a59a8918088aaf4ba48
SeokJu Kim <asdfg7123@gmail.com>
2024-06-10 18:23:48 +0900

    [Bronze V] Title: 立方体 (Cube), Time: 120 ms, Memory: 9588 KB -BaekjoonHub

---
+++ 백준/Bronze/24082. 立方体 （Cube）/立方体 （Cube）.js
@@ -0,0 +1,8 @@
+// https://www.acmicpc.net/problem/24082
+
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "0_example.txt";
+const splitPath = process.platform === "linux" ? "\n" : "\r\n";
+let input = fs.readFileSync(filePath).toString().trim().split(splitPath);
+
+console.log(parseInt(input[0]) ** 3);
