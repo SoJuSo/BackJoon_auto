6469e9f2bfcf96b1dd5e2c418fda8e9433145d36
SeokJu Kim <asdfg7123@gmail.com>
2024-06-10 18:15:49 +0900

    [Bronze V] Title: 時間 (Hour), Time: 96 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/27327. 時間 （Hour）/時間 （Hour）.js
@@ -0,0 +1,8 @@
+// https://www.acmicpc.net/problem/27327
+
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "0_example.txt";
+const splitPath = process.platform === "linux" ? "\n" : "\r\n";
+let input = fs.readFileSync(filePath).toString().trim().split(splitPath);
+
+console.log(Number(input[0]) * 24);
