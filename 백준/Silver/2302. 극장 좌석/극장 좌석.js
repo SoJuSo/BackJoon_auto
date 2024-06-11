b61865ec9fe72d761bc05e615f43985fcd4bc4a0
SeokJu Kim <asdfg7123@gmail.com>
2024-06-11 14:28:00 +0900

    [Silver I] Title: 극장 좌석, Time: 124 ms, Memory: 9592 KB -BaekjoonHub

---
+++ 백준/Silver/2302. 극장 좌석/극장 좌석.js
@@ -0,0 +1,33 @@
+//https://www.acmicpc.net/problem/2302
+
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "0_example.txt";
+const splitPath = process.platform === "linux" ? "\n" : "\r\n";
+let input = fs.readFileSync(filePath).toString().trim().split(splitPath);
+
+const N = +input.shift();
+const M = +input.shift();
+// 나머지 인풋이 고정석
+// 2 3 2 1 2 2 1 2 2
+
+// VIP가 분기점
+// VIP 사이는 합연산
+const dp = [];
+dp[0] = dp[1] = 1;
+dp[2] = 2;
+
+for (let i = 3; i <= N; i++) {
+  dp[i] = dp[i - 1] + dp[i - 2];
+}
+
+// console.log(dp);
+
+let cur = 0;
+const answer = [];
+for (let i = 0; i < M; i++) {
+  answer.push(dp[+input[i] - 1 - cur]);
+  cur = +input[i];
+}
+answer.push(dp[N - +input[M - 1]]);
+
+console.log(M === 0 ? dp[N] : answer.reduce((acc, cur) => acc * cur));
