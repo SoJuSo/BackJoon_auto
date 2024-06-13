e2ccfcdebde4d5b9c9ad354dc3c4f907239b5f3f
SeokJu Kim <asdfg7123@gmail.com>
2024-06-13 23:58:08 +0900

    [Bronze V] Title: 삼각형, Time: 124 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/29751. 삼각형/삼각형.js
@@ -0,0 +1,8 @@
+//https://www.acmicpc.net/problem/29751
+
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "0_example.txt";
+const splitPath = process.platform === "linux" ? "\n" : "\r\n";
+let input = fs.readFileSync(filePath).toString().trim().split(splitPath);
+
+console.log(((+input[0].split(" ")[0] * +input[0].split(" ")[1]) / 2).toFixed(1));
