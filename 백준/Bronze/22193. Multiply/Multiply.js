6e864a94cd073521d50c4d7c9d711997cc4e91d6
SeokJu Kim <asdfg7123@gmail.com>
2024-06-23 22:37:57 +0900

    [Bronze V] Title: Multiply, Time: 120 ms, Memory: 9332 KB, Score: 40 point -BaekjoonHub

---
+++ 백준/Bronze/22193. Multiply/Multiply.js
@@ -0,0 +1,8 @@
+// https://www.acmicpc.net/problem/22193
+
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "0_example.txt";
+const splitPath = process.platform === "linux" ? "\n" : "\r\n";
+let input = fs.readFileSync(filePath).toString().trim().split(splitPath);
+
+console.log(+input[1] * +input[2]);
