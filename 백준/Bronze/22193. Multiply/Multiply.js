67bfa326521167c28d81f49aec2cd775ad33e1d0
SeokJu Kim <asdfg7123@gmail.com>
2024-06-23 22:39:18 +0900

    [Bronze V] Title: Multiply, Time: 152 ms, Memory: 9612 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/22193. Multiply/Multiply.js
+++ 백준/Bronze/22193. Multiply/Multiply.js
@@ -5,4 +5,4 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "0_example.txt";
 const splitPath = process.platform === "linux" ? "\n" : "\r\n";
 let input = fs.readFileSync(filePath).toString().trim().split(splitPath);
 
-console.log(+input[1] * +input[2]);
+console.log((BigInt(input[1]) * BigInt(input[2])).toString());
