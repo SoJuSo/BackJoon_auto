334b2388b6812ebf84517355b76c55bd7b75c384
SeokJu Kim <asdfg7123@gmail.com>
2024-06-24 09:58:41 +0900

    [Silver III] Title: 통계학, Time: 984 ms, Memory: 56076 KB -BaekjoonHub

---
+++ 백준/Silver/2108. 통계학/통계학.js
@@ -0,0 +1,31 @@
+// https://www.acmicpc.net/problem/2108
+
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "0_example.txt";
+const splitPath = process.platform === "linux" ? "\n" : "\r\n";
+let input = fs.readFileSync(filePath).toString().trim().split(splitPath);
+
+const N = +input.shift();
+const obj = {};
+input.sort((a, b) => a - b);
+
+let sum = 0;
+for (let i = 0; i < N; i++) {
+  sum += +input[i];
+  if (obj[input[i]] === undefined) {
+    obj[input[i]] = 1;
+  } else {
+    obj[input[i]]++;
+  }
+}
+
+console.log(Math.round(sum / N) || 0);
+console.log(input[N - Math.ceil(N / 2)]);
+const arr = Object.entries(obj).sort((a, b) => {
+  if (a[1] === b[1]) {
+    return a[0] - b[0];
+  }
+  return b[1] - a[1];
+});
+console.log(arr.length === 1 ? arr[0][0] : arr[1][1] === arr[0][1] ? arr[1][0] : arr[0][0]);
+console.log(Math.max(...input) - Math.min(...input));
