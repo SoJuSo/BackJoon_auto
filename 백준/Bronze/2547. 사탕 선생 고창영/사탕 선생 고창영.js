a56cb99a2c3e919519bfb240c2f2793344881e8d
SeokJu Kim <asdfg7123@gmail.com>
2024-06-27 12:40:55 +0900

    [Bronze III] Title: 사탕 선생 고창영, Time: 192 ms, Memory: 13708 KB -BaekjoonHub

--- 백준/Bronze/2547. 사탕 선생 고창영/사탕 선생 고창영.js
+++ 백준/Bronze/2547. 사탕 선생 고창영/사탕 선생 고창영.js
@@ -1,4 +1,4 @@
-// https://www.acmicpc.net/problem/2547
+// https://www.acmicpc.net/problem/2845
 
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "0_example.txt";
@@ -6,9 +6,11 @@ const splitPath = process.platform === "linux" ? "\n" : "\r\n";
 let input = fs.readFileSync(filePath).toString().trim().split(splitPath);
 
 const TC = +input.shift();
+const answer = [];
 for (let i = 0; i < TC; i++) {
   input.shift();
   const N = +input.shift();
   const sum = input.splice(0, N).reduce((a, c) => BigInt(a) + BigInt(c) - BigInt(N), 0n);
-  sum % BigInt(N) === 0n ? console.log("YES") : console.log("NO");
+  sum % BigInt(N) === 0n ? answer.push("YES") : answer.push("NO");
 }
+console.log(answer.join("\n"));
