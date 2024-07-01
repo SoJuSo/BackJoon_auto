fe9cad376553efb49679f9a150052beebc6209f0
SeokJu Kim <asdfg7123@gmail.com>
2024-07-01 15:09:26 +0900

    [Silver I] Title: 포도주 시식, Time: 164 ms, Memory: 10516 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.js
@@ -0,0 +1,20 @@
+// https://www.acmicpc.net/problem/2156
+
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "0_example.txt";
+const splitPath = process.platform === "linux" ? "\n" : "\r\n";
+let input = fs.readFileSync(filePath).toString().trim().split(splitPath);
+
+const N = +input.shift();
+const list = input.map((v) => +v);
+
+const dp = Array.from(N + 1);
+dp[0] = 0;
+dp[1] = list[0];
+if (N > 1) dp[2] = list[0] + list[1];
+
+for (let i = 3; i <= N; i++) {
+  dp[i] = Math.max(dp[i - 1], dp[i - 3] + list[i - 2] + list[i - 1], dp[i - 2] + list[i - 1]);
+}
+
+console.log(Math.max(...dp));
