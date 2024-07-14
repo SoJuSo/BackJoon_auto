f9bb9ec026ac9b811ee2466cf68aefd165236ffc
SeokJu Kim <asdfg7123@gmail.com>
2024-07-14 22:11:10 +0900

    [Silver I] Title: 경로 찾기, Time: 168 ms, Memory: 9920 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.js
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.js
@@ -8,16 +8,22 @@ let input = fs.readFileSync(filePath).toString().trim().split(splitPath);
 // 1초
 // 메모리 적당
 // 플로이드 워셜 알고리즘
+// 간접 경로 계산도 해야하기에 플로이드 워셜이 적합
 
 const N = +input.shift(); // n = 정점의 개수
 const list = input.map((v) => v.split(" ").map(Number));
 
 const sol = (n, arr) => {
+  // 플로이드 워셜 알고리즘 구현
   for (let i = 0; i < n; i++) {
+    // 중간 정점
     for (let j = 0; j < n; j++) {
+      // 시작 정점
       for (let k = 0; k < n; k++) {
+        // 도착 정점
+        // j에서 i를 거쳐 k로 가는 경로가 있다면
         if (arr[j][i] && arr[i][k]) {
-          arr[j][k] = 1;
+          arr[j][k] = 1; // j에서 k로 가는 경로가 있음을 표시
         }
       }
     }
