d3f5ed9800984142705984c97fbfb69becf4f0bd
SeokJu Kim <asdfg7123@gmail.com>
2024-07-17 20:47:48 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 128 ms, Memory: 9572 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.js
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.js
@@ -1,36 +1,28 @@
 // https://www.acmicpc.net/problem/1316
 
-let fs = require("fs");
-const { type } = require("os");
-// let input = fs.readFileSync("example.txt").toString().split("\n"); //vscode
-let input = fs.readFileSync("/dev/stdin").toString().split("\n"); //백준
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "0_example.txt";
+const splitPath = process.platform === "linux" ? "\n" : "\r\n";
+let input = fs.readFileSync(filePath).toString().trim().split(splitPath);
 
-// console.log(input);
-//아이디어
-//주어진 횟수만큼 순회하며 문자열을 체크한다.
-// 우선 문자열을 배열로 나누어 순회하다가 처음 마주한 단어는 temp에 넣고 이전 단어와 현재 단어가 같으면 지속하고
-// 그렇지 않으면 false를 리턴하는 함수를 구현한다.
+const T = +input.shift();
 
-const N = Number(input[0]);
-input.shift();
-const words = input;
-let answer = 0;
+const sol = (n, arr) => {
+  let ans = 0;
+  for (let i = 0; i < n; i++) {
+    const cur = {};
+    arr[i].split("").forEach((val, index) => {
+      if (cur[val] === undefined) cur[val] = 1;
+      else {
+        if (cur[val] && arr[i][index] !== arr[i][index - 1]) {
+          cur[val]++;
+        }
+      }
+    });
+    const dup = [...new Set(Object.values(cur))];
+    dup.length === 1 && dup[0] === 1 ? ans++ : 0;
+  }
+  return ans;
+};
 
-// console.log(N, words);
-for (let i = 0; i < N; i++) {
-  let temp = [];
-  let checker = true;
-  words[i].split("").forEach((val, index) => {
-    // console.log(temp.includes(val), words[i].split("")[index - 1] !== val);
-    // console.log(val);
-    if (!temp.includes(val)) {
-      temp.push(val);
-    } else if (temp.includes(val) && words[i].split("")[index - 1] !== val) {
-      checker = false;
-    }
-  });
-  //   console.log(words[i], checker);
-  checker && answer++;
-}
-
-console.log(answer);
+console.log(sol(T, input));
