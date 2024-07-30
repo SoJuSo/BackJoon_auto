61b019b12d7770c55ba12f594d350852c5e20606
SeokJu Kim <asdfg7123@gmail.com>
2024-07-30 23:59:55 +0900

    [Silver IV] Title: 한수, Time: 136 ms, Memory: 9684 KB -BaekjoonHub

--- 백준/Silver/1065. 한수/한수.js
+++ 백준/Silver/1065. 한수/한수.js
@@ -4,35 +4,26 @@ let fs = require("fs");
 // let input = fs.readFileSync("e.txt").toString().split("\r\n"); //vscode
 let input = fs.readFileSync("/dev/stdin").toString().split("\n"); //백준
 
-//아이디어
-//등차수열, 각 자리수 간 규칙이 등차수열을 이룬다면 그 수를 한수라고 한다.
-
-const N = Number(input[0]); //주어진 input N
-let answer = 0; // 정답을 담는 변수 answer
+const N = Number(input[0]);
+let answer = 0;
 
 for (let i = 1; i <= N; i += 1) {
-  const arr = String(i).split(""); //비교해야 하므로 문자열화 해서 배열에 담아준다.
-  let TF = true; //한수인지 아닌지 판별해주는 bool 변수
+  const arr = String(i).split("");
+  let TF = true;
 
   let diff_1 = arr[1] - arr[0];
   for (let j = 0; j < arr.length; j++) {
-    //반복문을 돌며
     if (arr.length <= 2) {
-      //어차피 두 자리수까지는 무조건 맞다.
       break;
     } else {
-      //세자리수 ~ 네자리수
       if (j > 0) {
         let diff_2 = arr[j] - arr[j - 1];
         diff_1 != diff_2 ? (TF = false) : 0;
-        // console.log(arr, diff_1, diff_2, TF);
       }
     }
   }
-  //   console.log(arr, diff_1, diff_2, TF);
 
   TF ? answer++ : 0;
-  //   console.log(arr);
 }
 
 console.log(answer);
