c39d962c39c7c2d392240edad0d8c1df9b6cf21e
SeokJu Kim <asdfg7123@gmail.com>
2024-08-18 23:48:49 +0900

    [Silver V] Title: 돌 게임, Time: 144 ms, Memory: 9372 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.js
@@ -0,0 +1,14 @@
+// https://www.acmicpc.net/problem/9655
+
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "0_example.txt";
+const splitPath = process.platform === "linux" ? "\n" : "\r\n";
+let input = fs.readFileSync(filePath).toString().trim().split(splitPath);
+
+const N = +input.shift();
+
+const sol = (n) => {
+  return n % 2 === 0 ? "CY" : "SK";
+};
+
+console.log(sol(N));
