6076333dd2d4edf0d38c4b706b6f41cffc33a400
SeokJu Kim <asdfg7123@gmail.com>
2024-08-31 23:15:11 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 88 ms, Memory: 9476 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.js
@@ -0,0 +1,24 @@
+// https://www.acmicpc.net/problem/2446
+
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "0_example.txt";
+const splitPath = process.platform === "linux" ? "\n" : "\r\n";
+let input = fs.readFileSync(filePath).toString().trim().split(splitPath);
+
+const N = +input.shift();
+
+const sol = (n) => {
+  const answer = [];
+
+  for (let i = n; i > 0; i--) {
+    answer.push(" ".repeat(n - i) + "*".repeat(2 * i - 1));
+  }
+
+  for (let i = 2; i <= n; i++) {
+    answer.push(" ".repeat(n - i) + "*".repeat(2 * i - 1));
+  }
+
+  return answer.join("\n");
+};
+
+console.log(sol(N));
