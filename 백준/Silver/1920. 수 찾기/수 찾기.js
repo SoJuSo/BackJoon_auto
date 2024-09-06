a02608e992f24456ae6adea75db750ec55c94882
SeokJu Kim <asdfg7123@gmail.com>
2024-09-06 21:30:14 +0900

    [Silver IV] Title: 수 찾기, Time: 312 ms, Memory: 55860 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.js
+++ 백준/Silver/1920. 수 찾기/수 찾기.js
@@ -1,42 +1,28 @@
 // https://www.acmicpc.net/problem/1920
 
-let fs = require("fs");
-// let input = fs.readFileSync("example.txt").toString().split("\n"); //vscode
-let input = fs.readFileSync("/dev/stdin").toString().split("\n"); //백준
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "0_example.txt";
+const splitPath = process.platform === "linux" ? "\n" : "\r\n";
+let input = fs.readFileSync(filePath).toString().trim().split(splitPath);
 
-// console.log(input);
-const N = Number(input[0]);
-const N_arr = input[1].split(" ").map(Number);
-const M = Number(input[2]);
-const M_arr = input[3].split(" ").map(Number);
+const N = +input.shift();
+const Narr = input.shift().split(" ");
+const M = +input.shift();
+const Marr = input.shift().split(" ");
 
-N_arr.sort((a, b) => a - b);
+const sol = (listnum, listarr, targetnum, targetarr) => {
+  const answer = [];
 
-// console.log(N, M, N_arr, M_arr);
+  const obj = {};
+  listarr.forEach((val) => {
+    obj[val] === undefined ? (obj[val] = 1) : 0;
+  });
 
-// console.log();
-const binarySerach = (array, target) => {
-  let start = 0;
-  let end = array.length - 1;
+  targetarr.forEach((val) => {
+    obj[val] ? answer.push(1) : answer.push(0);
+  });
 
-  while (start <= end) {
-    let mid = parseInt((start + end) / 2);
-    if (target === array[mid]) {
-      return true;
-    } else {
-      if (target > array[mid]) {
-        start = mid + 1;
-      } else {
-        end = mid - 1;
-      }
-    }
-  }
-  return false;
+  return answer.join("\n");
 };
 
-const answer = [];
-for (let i = 0; i < M; i++) {
-  binarySerach(N_arr, M_arr[i]) ? answer.push(1) : answer.push(0);
-}
-
-console.log(answer.join(" "));
+console.log(sol(N, Narr, M, Marr));
