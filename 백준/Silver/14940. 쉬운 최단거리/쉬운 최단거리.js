e93e55942a891eef172361e7bcb70330a3798ccc
SeokJu Kim <asdfg7123@gmail.com>
2024-09-17 17:18:45 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 288 ms, Memory: 35404 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.js
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.js
@@ -1,4 +1,4 @@
-// https://www.acmicpc.net/problem/2747
+// https://www.acmicpc.net/problem/32314
 
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "0_example.txt";
@@ -8,58 +8,64 @@ let input = fs.readFileSync(filePath).toString().trim().split(splitPath);
 const [N, M] = input.shift().split(" ").map(Number);
 const list = input.map((v) => v.split(" ").map(Number));
 
-const bfs = (n, m, start, graph) => {
-  const answer = Array.from(Array(n), () => Array(m).fill(0));
-  const dx = [0, 1, 0, -1];
-  const dy = [1, 0, -1, 0];
-  const visited = Array.from(Array(n), () => Array(m).fill(0));
-  const queue = [];
-  queue.push(start);
+const sol = (col, row, gp) => {
+  // 아이디어
+  // 구현
 
-  while (queue.length) {
-    const cur = queue.shift();
-    const [y, x] = cur;
+  const bfs = (n, m, startNode, graph) => {
+    const dx = [1, -1, 0, 0],
+      dy = [0, 0, -1, 1];
+    const answer = Array.from(Array(n), () => Array(m).fill(0));
+    const visited = Array.from(Array(n), () => Array(m).fill(0));
 
-    for (let i = 0; i < 4; i++) {
-      if (
-        x + dx[i] >= 0 &&
-        x + dx[i] < M &&
-        y + dy[i] >= 0 &&
-        y + dy[i] < n &&
-        graph[y + dy[i]][x + dx[i]]
-      ) {
-        if (!visited[y + dy[i]][x + dx[i]]) {
-          answer[y + dy[i]][x + dx[i]] = answer[y][x] + 1;
-          queue.push([y + dy[i], x + dx[i]]);
-          visited[y + dy[i]][x + dx[i]] = 1;
+    const queue = [startNode];
+
+    while (queue.length) {
+      const [y, x] = queue.shift();
+
+      for (let i = 0; i < 4; i++) {
+        if (
+          x + dx[i] >= 0 &&
+          x + dx[i] < M &&
+          y + dy[i] >= 0 &&
+          y + dy[i] < N &&
+          graph[y + dy[i]][x + dx[i]]
+        ) {
+          if (!visited[y + dy[i]][x + dx[i]]) {
+            answer[y + dy[i]][x + dx[i]] = answer[y][x] + 1;
+            queue.push([y + dy[i], x + dx[i]]);
+            visited[y + dy[i]][x + dx[i]] = 1;
+          }
         }
       }
     }
-  }
 
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < m; j++) {
-      if (graph[i][j] === 1 && answer[i][j] === 0) {
-        answer[i][j] = -1;
+    // 시작점 보정
+    answer[start[0]][start[1]] = 0;
+
+    // 0보정
+    for (let i = 0; i < n; i++) {
+      for (let j = 0; j < m; j++) {
+        if (graph[i][j] === 1 && answer[i][j] === 0) {
+          answer[i][j] = -1;
+        }
       }
     }
-  }
-  answer[start[0]][start[1]] = 0;
 
-  return answer.map((v) => v.join(" ")).join("\n");
-};
+    return answer.map((v) => v.join(" ")).join("\n");
+  };
 
-const sol = (n, m, arr) => {
   let start = [0, 0];
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < m; j++) {
-      if (arr[i][j] === 2) {
+  for (let i = 0; i < col; i++) {
+    for (let j = 0; j < row; j++) {
+      if (gp[i][j] === 2) {
         start = [i, j];
+        break;
       }
     }
   }
 
-  return bfs(n, m, start, arr);
+  return bfs(col, row, start, gp);
 };
 
 console.log(sol(N, M, list));
